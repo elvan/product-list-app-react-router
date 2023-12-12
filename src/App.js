77861a99c75aca8e0e842e2c19b4d1857fba21e0
Elvan Hidayat <elvanhd@gmail.com>
2023-12-12 21:18:03 +0700

    showing error pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 
+import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
